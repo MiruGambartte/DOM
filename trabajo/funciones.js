4de16c0567e3c32a3f0a47de8d9e122ca98fdcdb
MiruGambartte <108378438+MiruGambartte@users.noreply.github.com>
2023-08-10 10:41:01 -0300

    Add files via upload

---
+++ trabajo/funciones.js
@@ -0,0 +1,14 @@
+function TamañoChico() {
+    let titC = document.getElementById('parrafo')
+    titC.style.fontSize = '10px'
+}
+
+function TamañoMediano() {
+    let titM = document.getElementById('parrafo')
+    titM.style.fontSize = '17px'
+}
+
+function TamañoGrande() {
+    let titG = document.getElementById('parrafo')
+    titG.style.fontSize = '26px'
+}
